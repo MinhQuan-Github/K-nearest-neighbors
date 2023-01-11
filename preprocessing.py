f3439a15578d25043baf68be58b4b24039fc10a5
Đỗ Minh Quân <minhquan.5322@aol.com>
2023-01-11 15:55:22 +0700

    Done preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+class Preprocessing:
+    def __init__(self):
+        self.data_path = 'datasets/the_trang.csv'
+        self.label_path = 'datasets/the_trang_labels.csv'
+        self.data = pd.read_csv(self.data_path).values
+        self.data = np.array(self.data, dtype=np.float32)
+        self.labels = pd.read_csv(self.label_path).values
+
+    def drawData(self, isShow=False, label=None):
+        plt.title("K-nearest-neighbors")
+        plt.xlabel("Heights")
+        plt.ylabel("Weights")
+        if label is None:
+            plt.scatter(self.data[:, 0], self.data[:, 1])
+        else:
+            plt.scatter(self.data[:, 0], self.data[:, 1], c=label)
+        if isShow:
+            plt.show()
+
+    def drawPoints(self, points, isShow=False, cs='rx'):
+        plt.plot(points[:, 0], points[:, 1], label=cs)
+        if isShow:
+            plt.show()
+
+    def normalization(self):
+        scaler = [1, 1]
+        scaler_save = []
+
+        for i in range(self.data.shape[1] - 1):
+            min_cols = np.min(self.data[:, i])
+            max_cols = np.max(self.data[:, i])
+            self.data[:, i] = scaler[i] * (self.data[:, i] - min_cols) / (max_cols - min_cols)
+            scaler_save.append([min_cols, max_cols])
+        scaler_save = np.array(scaler_save)
+        np.save('models/scaler_data.npy', scaler_save)
+        np.save('models/scaler_label.npy', self.labels)
+        print("normalize succeeded")
+
+    def get_data_training(self):
+        self.normalization()
+        return self.data, self.labels
+
+
+if __name__ == '__main__':
+    prep_objc = Preprocessing()
+    prep_objc.normalization()
+    print(prep_objc.labels)
+    prep_objc.drawData(isShow=True)
